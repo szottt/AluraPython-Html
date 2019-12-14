70fa0acacbf58af8371b355a0ec83d30dc69654f
szottt <szottt@hotmail.com>
2019-12-14 13:27:15 -0300

    incluindo palavras.txt

--- Python3/forca.py
+++ Python3/forca.py
@@ -7,29 +7,39 @@ def play():
     print('*** BEM VINDO AO JOGO ***')
     print('*' * 50)
 
-    secret_word = 'banana'
-    right_letters = ["_","_","_","_","_","_"]
+    secret_word = 'banana'.upper()
+    right_letters = ["_" for letter in secret_word]
 
-    enforcou = False
+    reinforce = False
     acertou = False
+    erros = 0
 
     print(right_letters)
 
-    while(not enforcou and not acertou):
-        kick = str(input('Digite uma letra: ')).strip
-        str = "this is string example....wow!!!";
-print "str.capitalize() : ", str.upper()
+    while(not reinforce and not acertou):
+        kick = input('Digite uma letra: ').strip().upper()
 
-        index = 0
-        for letra in secret_word:
-            if(kick.upper() == letra.upper):
-                right_letters[index] = letra
-            index += 1
-        
+        if(kick in secret_word):
+
+            index = 0
+            for letra in secret_word:
+                if(kick == letra):
+                    right_letters[index] = letra
+                index += 1
+        else:
+            erros += 1
+
+        reinforce = erros == 6
+
+        acertou = "_" not in right_letters
+
         print(right_letters)
-    
+        
+    if(acertou):
+        print('Voce ganhou')
+    else:
+        print('Voce errou')
     print('fim de jogo')
-
 
 if(__name__ == "__main__"):
     play()
