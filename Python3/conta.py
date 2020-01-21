10a32da6b820ef03a5f8fc900132754bd1481ee8
szottt <szottt@hotmail.com>
2020-01-20 21:09:15 -0300

    Implementando melhorias no codigo

---
+++ Python3/conta.py
@@ -0,0 +1,33 @@
+class Conta:
+
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto ... {}".format(self))
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print(f"Saldo {self.__saldo} do titular {self.__titular}")
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def saca(self, valor):
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+ 
+''' 
+class Retangulo:
+
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
+        self.__area = x * y
+
+    def obter_area(self):
+        return self.__area
+'''
